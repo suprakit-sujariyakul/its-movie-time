00b8271fe629365c716fb2655f36021c0a3bfe4d
Suprakit Sujariyakul <suprakitsujariyakul@gmail.com>
2023-02-04 17:27:25 +0700

    deploy

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main>
       <div className='grid gap-16 grid-cols-fluid'>
-        {res?.results?.map((movie) => (
+        {res.results?.map((movie) => (
           <Movie
             key={movie.id}
             id={movie.id}
